531912d87c73c00a8ac6cbdc1cc3b0ad432d44ed
Aakash Ojha <64247310+theojha01@users.noreply.github.com>
2021-07-09 14:59:51 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -3,3 +3,13 @@ provider "aws" {
   shared_credentials_file = var.creds
   profile = "default"
 }
+
+resource "aws_instance" "os1" {
+ami = "ami-010aff33ed5991201"
+instance_type = "t2.micro"
+security_groups = [ "launch-wizard-3" ]
+ key_name = "terraform_trial"
+ tags = {
+  Name = "Webserver"
+ }
+}
